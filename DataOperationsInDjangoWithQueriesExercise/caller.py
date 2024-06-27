a51bffe7b98e39832cf2d111f3176d4c99e1830d
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2024-06-27 17:25:05 +0300

    added more solutions

---
+++ DataOperationsInDjangoWithQueriesExercise/caller.py
@@ -0,0 +1,10 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+
+# Create queries within functions
